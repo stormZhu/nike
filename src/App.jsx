86932326ecfdb5544e8c51d0224da8216ee00cab
zhuyuqing <779724979@qq.com>
2026-01-05 00:40:54 +0800

    初步实现nav

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,35 @@ import {
   Subscribe,
   SuperQuality,
 } from "./sections";
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
     <main className="relative">
-      Nav
+      <Nav />
       <section className="xl:padding-1 wide:padding-r padding-b">
-        <Hero></Hero>
+        <Hero />
       </section>
       <section className="padding">
-        <PopularProducts></PopularProducts>
+        <PopularProducts />
       </section>
       <section className="padding">
-        <SuperQuality></SuperQuality>
+        <SuperQuality />
       </section>
       <section className="padding-x py-10">
-        <Services></Services>
+        <Services />
       </section>
       <section className="padding">
-        <SpecialOffer></SpecialOffer>
+        <SpecialOffer />
       </section>
       <section className="bg-pale-blue padding">
-        <CustomerReviews></CustomerReviews>
+        <CustomerReviews />
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
-        <Subscribe></Subscribe>
+        <Subscribe />
       </section>
       <section className="bg-black padding-x padding-t pb-8">
-        <Footer></Footer>
+        <Footer />
       </section>
     </main>
   );
