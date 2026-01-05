78b6203504b1ca7a1be1bde45d16ac163b69eb45
zhuyuqing <779724979@qq.com>
2026-01-06 01:16:13 +0800

    hero中的右侧图片

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
 import { statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
 
 const Hero = () => {
   return (
@@ -8,7 +9,7 @@ const Hero = () => {
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="relative xl:2-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      <div className="relative flex flex-col xl:w-2/5 justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-monstserrat text-coral-red">
           Our Summer collections
         </p>
@@ -34,6 +35,16 @@ const Hero = () => {
             </div>
           ))}
         </div>
+      </div>
+
+      <div className="relative flex flex-1 justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoe1}
+          alt="shoe collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
       </div>
     </section>
   );
