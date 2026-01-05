815770cae369ed212dae52850020027dd6db34a0
zhuyuqing <779724979@qq.com>
2026-01-06 00:28:45 +0800

    实现hero中的statistics

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
+import { statistics } from "../constants";
 
 const Hero = () => {
   return (
@@ -8,18 +9,31 @@ const Hero = () => {
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:2-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer collections</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p className="text-xl font-monstserrat text-coral-red">
+          Our Summer collections
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span>Nike</span>
-          Shoes
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
-        <p>
+        <p className="font-montserrat text-lg text-slate-gray leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
         <Button label="Show now" iconURL={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((item) => (
+            <div key={item.label}>
+              <p className="text-4xl font-palanquin font-bold">{item.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {item.label}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
