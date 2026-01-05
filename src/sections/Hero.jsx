100c4e0c879b83d0a7882152c74b5290dff66d48
zhuyuqing <779724979@qq.com>
2026-01-06 01:49:57 +0800

    完成hero右侧的shoeCard的ui

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import Button from "../components/Button";
+import ShoeCard from "../components/ShoeCard";
 import { arrowRight } from "../assets/icons";
-import { statistics } from "../constants";
+import { statistics, shoes } from "../constants";
 import { bigShoe1 } from "../assets/images";
 
 const Hero = () => {
@@ -45,6 +46,17 @@ const Hero = () => {
           height={500}
           className="object-contain relative z-10"
         />
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={() => {}}
+                bigShoeImg=""
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
