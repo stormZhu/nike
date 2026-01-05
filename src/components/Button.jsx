f47e650ece7faf754806c5c0ac376a420db95d2e
zhuyuqing <779724979@qq.com>
2026-01-06 00:05:15 +0800

    实现hero中的button

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({ label, iconURL }) => {
+  return (
+    <button className="flex justify-center items-center gap-2 px-7 py-4 font-monstserrat text-lg leading-none rounded-full text-white bg-coral-red border-coral-red">
+      {label}
+      <img
+        src={iconURL}
+        alt="arrow right icon"
+        className="ml-2 rounded-full size-5"
+      />
+    </button>
+  );
+};
+
+export default Button;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,28 @@
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:2-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer collections</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="Show now" iconURL={arrowRight} />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
